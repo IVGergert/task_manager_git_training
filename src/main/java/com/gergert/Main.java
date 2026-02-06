4ee6d2e8da9578297b9dedc9d6304199a375d62b
IlyaGergert <IlyaGergert@gmail.com>
2026-02-06 15:39:41 +0300

    Add new logic for TaskService class

--- src/main/java/com/gergert/Main.java
+++ src/main/java/com/gergert/Main.java
@@ -1,23 +1,7 @@
 package com.gergert;
 
-import com.gergert.service.TaskService;
-
-import java.util.Scanner;
-
 public class Main {
-    private static final TaskService taskService = new TaskService();
-    private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        System.out.println("Welcome to Task Manager!");
-
-        System.out.println("Choose a response option:");
-        System.out.println("1. View all tasks");
-        System.out.println("2. Add new task");
-        System.out.println("3. Delete task by id");
-        int option = scanner.nextInt();
-
-
 
     }
 
--- src/main/java/com/gergert/service/TaskService.java
+++ src/main/java/com/gergert/service/TaskService.java
@@ -9,34 +9,45 @@ public class TaskService {
     private List<Task> tasks = new ArrayList<>();
 
     public void addTask(String description){
-        tasks.add(new Task(description));
-        System.out.println("Success: Task added!");
+        System.out.println("--- ADD NEW TASK ---");
+
+        Task newTask = new Task(description);
+        tasks.add(newTask);
+        System.out.println("Success: Added task:" + newTask.getDescription() + " with id: " + newTask.getId());
     }
 
     public void printAllTasks() {
         System.out.println("--- YOUR TASKS ---");
 
-        for (Task task : tasks) {
-            System.out.println(task.toString());
+        if (tasks.isEmpty()) {
+            System.out.println("List is empty.");
+        } else {
+            for (Task task : tasks) {
+                System.out.println(task.toString());
+            }
         }
     }
 
-    public void deleteTaskById(int id) {
-        System.out.println("--- Delete TASK ---");
+    public void deleteTask(int id) {
+        System.out.println("--- REMOVE TASK ---");
 
         Task taskToRemove = null;
-        for (Task task : tasks){
-            if (task.getId() == id){
+
+        for (Task task : tasks) {
+            if (task.getId() == id) {
                 taskToRemove = task;
                 break;
             }
         }
 
-        if (taskToRemove != null){
-            tasks.remove(id);
+        if (taskToRemove != null) {
+            tasks.remove(taskToRemove);
             System.out.println("Task with ID " + id + " deleted.");
         } else {
             System.out.println("Task with ID " + id + " not found!");
         }
     }
+
+
+
 }
