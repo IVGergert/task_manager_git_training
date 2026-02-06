536f019adaab71a57faedb36a0fdbe23e687eae4
IlyaGergert <IlyaGergert@gmail.com>
2026-02-06 15:12:47 +0300

    Add new variable id for Task

--- src/main/java/com/gergert/entity/Task.java
+++ src/main/java/com/gergert/entity/Task.java
@@ -3,12 +3,20 @@ package com.gergert.entity;
 import java.util.StringJoiner;
 
 public class Task {
+    private static int idCounter = 1;
+
+    private int id;
     private String description;
     private boolean isDone;
 
     public Task(String description) {
+        this.id = idCounter++;
         this.description = description;
         this.isDone = false;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getDescription() {
@@ -22,8 +30,9 @@ public class Task {
     @Override
     public String toString() {
         return new StringJoiner(", ", Task.class.getSimpleName() + "[", "]")
-                .add("description = '" + description + "'")
-                .add("isDone = " + isDone)
+                .add("id=" + id)
+                .add("description='" + description + "'")
+                .add("isDone=" + isDone)
                 .toString();
     }
 }
