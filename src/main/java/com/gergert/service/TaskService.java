3110b8b97e273e00b03d5154329da58bededcaf4
IlyaGergert <IlyaGergert@gmail.com>
2026-02-06 14:40:27 +0300

    Init TaskService class

---
+++ src/main/java/com/gergert/service/TaskService.java
@@ -0,0 +1,10 @@
+package com.gergert.service;
+
+import com.gergert.entity.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskService {
+    private List<Task> tasks = new ArrayList<>();
+}
